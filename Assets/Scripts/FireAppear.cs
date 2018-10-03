19c3221b8c1bf2ecdf5ed0ffc698440f405f40f6
cross <ne280107@senshu-u.jp>
2018-10-03 15:26:11 +0900

    クソゲー

---
+++ Assets/Scripts/FireAppear.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAppear : MonoBehaviour {
+    [SerializeField]
+    private GameObject fireAppear;
+    [SerializeField]
+    private GameObject[] fire;
+    private float firingInterval = 3.0f;
+    private int beforeNum = 0;
+    private int appearNum = 0;
+
+	private void Start () 
+    {
+        foreach (Transform item in fireAppear.transform)
+        {
+            item.gameObject.SetActive(false);
+        }
+        StartCoroutine( Firing() );
+	}
+
+    IEnumerator Firing()
+    {
+        while(true)
+        {
+            while (appearNum == beforeNum)
+            {
+                appearNum = Random.Range(0, 9);
+            }
+            fire[appearNum].SetActive(true);
+            beforeNum = appearNum;
+            yield return new WaitForSeconds(firingInterval);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private int digestionCount = 0;
+	
+	private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { this.transform.position += Vector3.forward * 2.0f; }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { this.transform.position += Vector3.back * 2.0f; }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { this.transform.position += Vector3.right * 2.0f; }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { this.transform.position += Vector3.left * 2.0f; }
+        if (Input.GetKeyDown(KeyCode.Space)) { digestionCount++; }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Fire") { digestionCount = 0; }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Fire")
+        {
+            if (digestionCount == 10) { other.gameObject.SetActive(false); }
+        }
+    }
+}

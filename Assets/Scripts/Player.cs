0c02bcf1b6c0ac0b36967db43167cb5a350903f4
cross <ne280107@senshu-u.jp>
2018-10-24 13:07:05 +0900

    ステージの見た目の微調整とエリアの設定

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     private int digestionCount = 0;
+    private Vector3 pos;
+    private static float stageLeftLine = -3.5f;
+    private static float stageRightLine = 0.5f;
+    private static float stageTopLine = 0.5f;
+    private static float stageBottomLine = -3.5f;
 	
 	private void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) { this.transform.position += Vector3.forward * 1.0f; }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) { this.transform.position += Vector3.back * 1.0f; }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) { this.transform.position += Vector3.right * 1.0f; }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) { this.transform.position += Vector3.left * 1.0f; }
+        Move();
         if (Input.GetKeyDown(KeyCode.Space)) { digestionCount++; }
 	}
 
@@ -23,7 +25,57 @@ public class Player : MonoBehaviour {
     {
         if (other.gameObject.tag == "Fire")
         {
+            // 10回ボタンを押したら火を消す
             if (digestionCount == 10) { other.gameObject.SetActive(false); }
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 targetPos;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            this.transform.position += Vector3.forward * 1.0f;
+            //targetPos = this.transform.position + Vector3.forward * 1.0f;
+            //if (!CanMoveCheck(this.transform.position, targetPos - this.transform.position)) return;
+
+            //this.transform.position = targetPos;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 90.0f, 0);
+            this.transform.position += Vector3.right * 1.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 180.0f, 0);
+            this.transform.position += Vector3.back * 1.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 270.0f, 0);
+            this.transform.position += Vector3.left * 1.0f;
+        }
+
+        pos = this.transform.position;
+
+        this.transform.position = new Vector3(Mathf.Clamp(pos.x, stageLeftLine, stageRightLine), pos.y, Mathf.Clamp(pos.z, stageBottomLine, stageTopLine));
+    }
+
+    private bool CanMoveCheck(Vector3 _nowPos, Vector3 _direction)
+    {
+        Debug.Log(_direction);
+        RaycastHit hit;
+        Physics.Raycast(_nowPos, _direction, out hit, 1);
+        if (hit.collider.gameObject.tag == "wood")
+        {
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 }
